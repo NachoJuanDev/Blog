d271a689b6cab6e74a381f8b3e69ec254c078086
Juan Salas-Morales <jisalas1@uc.cl>
2024-03-27 00:07:22 -0300

    :rocket: config custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://nachojuandev.github.io',
+	site: 'https://nachojuan.dev',
 	integrations: [mdx(), sitemap()],
 });
